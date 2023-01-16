bdb9354edb1e9c6f59d43be9dabf5dd44be9b6fa
Neslihan <116304785+Neslihan110@users.noreply.github.com>
2023-01-16 22:55:12 +0300

    Create Script.js

---
+++ modül2-çözüm/Script.js
@@ -0,0 +1,20 @@
+
+(function () {
+
+var names = ["Yakov", "John", "Jen", "Jason", "Paul", "Frank", "Lary", "Paula", "Laura", "Jim"];
+
+
+for (var i = 0; i < names.length; i++) {
+
+ 
+  var firstLetter = names[i].charAt(0).toLowerCase();
+
+ 
+  if (firstLetter === 'j') {
+    byeSpeaker.speak(names[i]);
+  } else {
+    helloSpeaker.speak(names[i]);
+  }
+}
+
+})();
